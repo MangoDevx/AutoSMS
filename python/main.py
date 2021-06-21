ac6935b6bb9513be7f0d9cb44ab9db6a0923b205
Keith <49873540+MangoDevx@users.noreply.github.com>
2021-06-21 18:02:13 -0400

    Updated main server + gitignore

--- python/main.py
+++ python/main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-
 import asyncio
 import websockets
+from chatbot.chatbot import execute_chatbot as exec_cbot
 
 async def server(websocket, path):
     while True:
         data = await websocket.recv()
         print('Data received: '+data)
-        await websocket.send('Feed: '+data)
+        result = exec_cbot(data)
+        await websocket.send('Reply: '+result)
 
 ADDRESS = "192.168.1.8"
 socket_server = websockets.serve(server, ADDRESS, 6789)
-print(f'Listening on {ADDRESS}:6789')
+print("Waiting for incoming data...")
 asyncio.get_event_loop().run_until_complete(socket_server)
 asyncio.get_event_loop().run_forever()
